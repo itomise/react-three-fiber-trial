c18107c9119f0d69aa25b33f85b3c216f99d5b92
ryota.ito <ryota.ito@sonicjam.co.jp>
2019-12-20 22:52:48 +0900

    add ほぼなにもしてないけど

--- src/work.js
+++ src/work.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
 import { css, jsx } from '@emotion/core'
+import { Utils } from './utils'
 
 
 const theme = css`
@@ -11,23 +12,23 @@ const theme = css`
 const theme1 = {
   width: '100vw',
   height: '100vh',
-  backgroundColor: '#ffffff'
+  backgroundColor: Utils.rgbTo16(249, 233, 209)
 }
 
 const Thing = () => {
   const ref = useRef()
 
   useFrame( ({ clock }) => {
-    ref.current.position.x += Math.cos(clock.getElapsedTime()) * 3
-    ref.current.position.y += Math.sin(clock.getElapsedTime()) * 3
-    ref.current.position.z += Math.cos(clock.getElapsedTime()) * 3
+    ref.current.position.x += Math.cos(clock.getElapsedTime()) * 4
+    ref.current.position.y += Math.sin(clock.getElapsedTime()) * 4
+    ref.current.position.z += Math.cos(clock.getElapsedTime()) * 4
     ref.current.rotation.y += 0.01
   })
 
   return (
     <mesh ref={ref}>
       <sphereGeometry attach='geometry' args={[300, 30, 30]} />
-      <meshStandardMaterial attach='material' color='#ff0000' />
+      <meshStandardMaterial attach='material' color={Utils.rgbTo16(233, 164, 49)} />
     </mesh>
   )
 }
@@ -37,12 +38,17 @@ const Work = () => {
     <div css={theme} style={theme1}>
       <Canvas camera={{ position: [0, 0, 1000] }}>
         <ambientLight
-          color="ffffff"
+          color={Utils.rgbTo16(255, 255, 255)}
+          intensity={1}
         />
         <pointLight
           color='#ffffff'
-          intensity={1}
-          position={[0, 2000, 1000]}
+          intensity={0.2}
+          position={[10000, 2000, 2000]}
+        />
+        <hemisphereLight
+          color='#ffffff'
+          intensity={0.8}
         />
         <Thing />
       </Canvas>
